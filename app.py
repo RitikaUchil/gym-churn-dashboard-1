13f97f2c03b1f3a287b9cf222565b12fc9518622
RitikaUchil <ritikauchil@gmail.com>
2025-12-26 16:02:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def set_background(image_path):
         .block-container {{
             background: rgba(255, 255, 255, 0.35);
             backdrop-filter: blur(1px);
-            -webkit-backdrop-filter: blur(12px);
+            -webkit-backdrop-filter: blur(1px);
             padding: 2rem;
             border-radius: 18px;
             box-shadow: 0 8px 30px rgba(0, 0, 0, 0.25);
